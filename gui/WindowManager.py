d9ddf806a9a9d0908089b68bf5e483a6f6dc13f5
flashifloosh <flashimcflash@gmail.com>
2025-01-21 11:11:49 +0100

    fix NoneType error for last_location on close

--- gui/WindowManager.py
+++ gui/WindowManager.py
@@ -1,11 +1,9 @@
 import FreeSimpleGUI as sg
 
-
 class WindowManager:
     last_location = (None, None)
     window = None
 
-    # Standard grafische Oberfläche wird generiert, die dann in den entsprechenden Klassen angepasst werden (=Grundgerüst).
     @classmethod
     def update(cls, new_layout, event_handler, size=(400, 100), r_loc=(0, 0)):
         if cls.window is not None:
@@ -15,20 +13,18 @@ class WindowManager:
                                relative_location=r_loc)
         cls.event_handler = event_handler
 
-    # Das Fenster wird immer an der gleichen Position generiert.
-    # Insofern das Fenster verschoben wird, wird an der gleichen Stelle die neue grafische Oberfläche geöffnet.
     @classmethod
     def run(cls):
         from gui.Startpage import Startpage
         cls.update(Startpage.get_layout(), Startpage.event_handler)
         while True:
             event, values = cls.window.read()
-            cls.last_location = cls.window.CurrentLocation()
+            if cls.window is not None and cls.window.TKroot.winfo_exists():
+                cls.last_location = cls.window.CurrentLocation()
             if event == sg.WIN_CLOSED:
                 break
             cls.event_handler(event, values)
 
-    # Wichtig, damit der event_handler überschrieben werden kann von anderen Klassen
     @classmethod
     def event_handler(cls, event, values):
         pass
